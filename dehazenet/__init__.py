519bc3556c1259a8dc7718a0e5fb0dbea9280423
Unk1ndled <83562508+Unk1ndledAC@users.noreply.github.com>
2025-11-19 22:16:22 +0800

    Update version to 0.1.2 and maintain imports

--- dehazenet/__init__.py
+++ dehazenet/__init__.py
@@ -1,6 +1,7 @@
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 from .models import DehazeNet
 from .losses import loss_dehaze
 from .dataset import HazyDataset
+
 from .metrics import calc_psnr, calc_ssim
