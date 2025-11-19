7d9fcfa9424ecbecd56dc83002ff7b45357b88c7
Unk1ndled <83562508+Unk1ndledAC@users.noreply.github.com>
2025-11-19 22:17:02 +0800

    Add SEModule and GhostModule to DehazeNet

--- dehazenet/models.py
+++ dehazenet/models.py
@@ -1,5 +1,48 @@
 import torch.nn as nn
+import math, torch
 
+class SEModule(nn.Module):
+    def __init__(self, channels, reduction=4):
+        super(SEModule, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc1 = nn.Conv2d(channels, channels // reduction, kernel_size=1, padding=0)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Conv2d(channels // reduction, channels, kernel_size=1, padding=0)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        module_input = x
+        x = self.avg_pool(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.sigmoid(x)
+        return module_input * x
+
+class GhostModule(nn.Module):
+    def __init__(self, inp, oup, kernel_size=1, ratio=2, dw_size=3, stride=1, relu=True):
+        super(GhostModule, self).__init__()
+        self.oup = oup
+        init_channels = math.ceil(oup / ratio)
+        new_channels = init_channels * (ratio - 1)
+
+        self.primary_conv = nn.Sequential(
+            nn.Conv2d(inp, init_channels, kernel_size, stride, kernel_size//2, bias=False),
+            nn.BatchNorm2d(init_channels),
+            nn.ReLU(inplace=True) if relu else nn.Sequential(),
+        )
+        self.cheap_operation = nn.Sequential(
+            nn.Conv2d(init_channels, new_channels, dw_size, stride, dw_size//2, groups=init_channels, bias=False),
+            nn.BatchNorm2d(new_channels),
+            nn.ReLU(inplace=True) if relu else nn.Sequential(),
+        )
+
+    def forward(self, x):
+        x1 = self.primary_conv(x)
+        x2 = self.cheap_operation(x1)
+        out = torch.cat([x1, x2], dim=1)
+        return out[:, :self.oup, :, :]
+    
 class ResBlock(nn.Module):
     def __init__(self, ch=32):
         super().__init__()
@@ -11,16 +54,18 @@ class ResBlock(nn.Module):
     def forward(self, x):
         return x + self.pipeline(x)
 
-
 class DehazeNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.pipeline = nn.Sequential(
             nn.Conv2d(3, 32, 3, 1, 1),
-            ResBlock(32), ResBlock(32),
+            GhostModule(32, 32),
+            SEModule(32),
+            ResBlock(32), ResBlock(32), 
             ResBlock(32),
             ResBlock(32), ResBlock(32),
             nn.Conv2d(32, 3, 3, 1, 1)
         )
     def forward(self, x):
         return self.pipeline(x)
+        
