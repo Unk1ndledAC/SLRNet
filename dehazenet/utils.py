e18ebf6d877fe3aee73e1196679f741265c02192
Unk1ndled <83562508+Unk1ndledAC@users.noreply.github.com>
2025-11-19 22:17:25 +0800

    Refactor data pairing logic into get_pairs function

--- dehazenet/utils.py
+++ dehazenet/utils.py
@@ -1,12 +1,12 @@
-import os, glob, random, torch
+import os, glob, random
 
-def make_split(root, ratio=(0.8, 0.1, 0.1), seed=1234, save_txt=True):
-    assert sum(ratio) == 1.0
-    random.seed(seed)
-    gt_list = sorted(glob.glob(os.path.join(root, 'gt', '*.*')))
+def get_pairs(root_dir):
+    gt_dir   = os.path.join(root_dir, 'gt')
+    hazy_dir = os.path.join(root_dir, 'hazy')
+    gt_list  = sorted(glob.glob(os.path.join(gt_dir, '*')))
     hazy_dict = {}
-    for p in glob.glob(os.path.join(root, 'hazy', '*.*')):
-        key = os.path.basename(p).split('.')[0]
+    for p in glob.glob(os.path.join(hazy_dir, '*')):
+        key = os.path.splitext(os.path.basename(p))[0]
         hazy_dict.setdefault(key, []).append(p)
 
     pairs = []
@@ -14,36 +14,4 @@ def make_split(root, ratio=(0.8, 0.1, 0.1), seed=1234, save_txt=True):
         key = os.path.splitext(os.path.basename(gp))[0]
         if key in hazy_dict:
             pairs.append((gp, random.choice(hazy_dict[key])))
-
-    random.shuffle(pairs)
-    n_train = int(len(pairs) * ratio[0])
-    n_val = int(len(pairs) * ratio[1])
-    split = {
-        'train': pairs[:n_train],
-        'val': pairs[n_train:n_train + n_val],
-        'test': pairs[n_train + n_val:]
-    }
-    if save_txt:
-        for phase, lst in split.items():
-            with open(os.path.join(root, f'split_{phase}.txt'), 'w') as f:
-                f.writelines([f'{g},{h}\n' for g, h in lst])
-    return split
-
-
-@torch.no_grad()
-def validate(net, val_pairs, device, crop=0):
-    from .dataset import HazyDataset
-    from .metrics import calc_psnr, calc_ssim
-    from torch.utils.data import DataLoader
-
-    net.eval()
-    val_set = HazyDataset(val_pairs, crop=crop)
-    loader = DataLoader(val_set, batch_size=1, shuffle=False)
-    psnr_sum = ssim_sum = n = 0
-    for I, J in loader:
-        I, J = I.to(device), J.to(device)
-        J_hat = net(I).clamp(0, 1)
-        psnr_sum += calc_psnr(J_hat, J)
-        ssim_sum += calc_ssim(J_hat, J)
-        n += 1
-    return psnr_sum / n, ssim_sum / n
+    return pairs
