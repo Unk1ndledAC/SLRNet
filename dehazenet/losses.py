d6853d4e5fdc8ee237bedbaf2378496e141e6458
Unk1ndled <83562508+Unk1ndledAC@users.noreply.github.com>
2025-11-21 16:19:40 +0800

    Refactor loss functions and VGG feature extraction

--- dehazenet/losses.py
+++ dehazenet/losses.py
@@ -29,28 +29,3 @@ def loss_freq(x, y):
 def loss_dehaze(J_hat, J):
     l1 = F.l1_loss(J_hat, J)
     return l1 + lambda_perc * loss_perc(J_hat, J) + lambda_freq * loss_freq(J_hat, J)
-
-"""
-from .models import ResBlock
-
-lambda_res = 0.5
-
-class ResFeat(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.pipeline = nn.Sequential(
-            nn.Conv2d(3, 32, 3, 1, 1),
-            *[ResBlock(32) for _ in range(9)]
-        )
-        self.eval()
-        for p in self.parameters():
-            p.requires_grad = False
-
-    def forward(self, x):
-        return self.pipeline(x)
-
-res_feat = ResFeat().to(device)
-
-def loss_res(x, y): 
-    return F.mse_loss(res_feat(x), res_feat(y))
-"""
