9d8ab7568c23813ef22f52b1a1c7133cccc1d0ad
Unk1ndled <83562508+Unk1ndledAC@users.noreply.github.com>
2025-11-23 23:06:21 +0800

    Add get_pairs_multi function for multi-image pairing
    Added a new function to get multiple pairs of images from clear and hazy directories.

--- dehazenet/utils.py
+++ dehazenet/utils.py
@@ -15,3 +15,26 @@ def get_pairs(root_dir):
         if key in hazy_dict:
             pairs.append((gp, random.choice(hazy_dict[key])))
     return pairs
+
+def get_pairs_multi(root_dir):
+    gt_dir   = os.path.join(root_dir, 'clear')
+    hazy_dir = os.path.join(root_dir, 'haze')
+
+    gt_list = sorted(glob.glob(os.path.join(gt_dir, '*.*'))) 
+
+    hazy_dict = {}
+    for hazy_path in glob.glob(os.path.join(hazy_dir, '*.*')):
+        basename = os.path.splitext(os.path.basename(hazy_path))[0]
+        
+        key = basename.split('_')[0]
+        hazy_dict.setdefault(key, []).append(hazy_path)
+        
+    pairs = []
+    for gt_path in gt_list:
+        key = os.path.splitext(os.path.basename(gt_path))[0]
+        if key in hazy_dict:
+            for hazy_path in hazy_dict[key]:
+                pairs.append((gt_path, hazy_path))
+                
+    print(f'Total pairs: {len(pairs)}')
+    return pairs
